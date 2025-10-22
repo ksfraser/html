9e7ed2f41dd8a9ed0ef12937bc2d06ff7d7f55bf
Kevin Fraser <54461925+ksfraser@users.noreply.github.com>
2025-10-22 14:25:27 -0600

    Implement HtmlEmail class with email validation
    Added email validation and constructor for creating email links.

--- src/Ksfraser/HTML/HtmlEmail.php
+++ src/Ksfraser/HTML/HtmlEmail.php
@@ -4,7 +4,45 @@ namespace Ksfraser\HTML\HTMLAtomic;
 
 use Ksfraser\HTML\HtmlElementInterface;
 
-class HtmlEmail extends HtmlLink
+/**//****************************
+* Email Links
+*
+* Creates: <a href="mailto:email@example.com">LINK TEXT</a>
+*
+* Email addresses are validated and automatically prefixed with "mailto:"
+* Extends HtmlA since email links are just specialized anchor tags
+*/
+class HtmlEmail extends HtmlA
 {
-	//we could validate the validity of the email address
+	/**
+	 * Create an email link
+	 * 
+	 * @param string $emailAddress The email address (without mailto:)
+	 * @param HtmlElementInterface|string|null $linkContent The content to display. 
+	 *        - HtmlElementInterface: any HTML element (text, image, etc.)
+	 *        - string: will be wrapped in HtmlString
+	 *        - null: email address will be used as link text
+	 * @param bool $validateEmail Whether to validate the email format (default: true)
+	 * @throws \Exception if email is invalid or content type is invalid
+	 */
+	function __construct( string $emailAddress, $linkContent = null, bool $validateEmail = true )
+	{
+		// Validate email address if requested
+		if( $validateEmail && !filter_var( $emailAddress, FILTER_VALIDATE_EMAIL ) )
+		{
+			throw new \Exception( "Invalid email address: $emailAddress" );
+		}
+		
+		// Build mailto URL
+		$mailtoUrl = "mailto:" . $emailAddress;
+		
+		// If no content provided, use email address as link text
+		if( $linkContent === null )
+		{
+			$linkContent = $emailAddress;
+		}
+		
+		// Call parent HtmlA constructor - it handles string/HtmlElementInterface/null
+		parent::__construct( $mailtoUrl, $linkContent );
+	}
 }
