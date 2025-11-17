7e3b73f06bab8854e0c3c9efc634cc916a30fea3
Kevin Fraser <fraser.ks@gmail.com>
2025-11-16 23:56:24 -0700

    Add table manipulation functions to FaUiFunctions class

--- src/Ksfraser/HTML/FaUiFunctions.php
+++ src/Ksfraser/HTML/FaUiFunctions.php
@@ -45,4 +45,44 @@ class FaUiFunctions {
             echo "</table>\n";
         }
     }
+
+    public static function table_header($labels, $params='')
+    {
+        if (function_exists('\\table_header')) {
+            call_user_func('\\table_header', $labels, $params);
+        } else {
+            echo "<tr>";
+            foreach ($labels as $label) {
+                echo "<th $params>$label</th>";
+            }
+            echo "</tr>\n";
+        }
+    }
+
+    public static function start_row($param="")
+    {
+        if (function_exists('\\start_row')) {
+            call_user_func('\\start_row', $param);
+        } else {
+            echo "<tr $param>\n";
+        }
+    }
+
+    public static function end_row()
+    {
+        if (function_exists('\\end_row')) {
+            call_user_func('\\end_row');
+        } else {
+            echo "</tr>\n";
+        }
+    }
+
+    public static function label_cells($label, $value, $params="")
+    {
+        if (function_exists('\\label_cells')) {
+            call_user_func('\\label_cells', $label, $value, $params);
+        } else {
+            echo "<td $params>$label</td><td $params>$value</td>\n";
+        }
+    }
 }
